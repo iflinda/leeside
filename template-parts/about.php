841db4983d48f455d4e9f4f2a8333d1e4008f060
iflinda <informlinda@gmail.com>
2025-08-06 00:35:15 -0700

    Removed comments.

--- template-parts/about.php
+++ template-parts/about.php
@@ -1,8 +1,6 @@
 <?php
-// Get the About page object by title
 $about_page = get_page_by_title('About');
 
-// Check and render the content
 if ($about_page) {
     echo apply_filters('the_content', $about_page->post_content);
 } else {
