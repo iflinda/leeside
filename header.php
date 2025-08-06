9a3a5363bbfd7ff2f383631c05f2fadb34582031
iflinda <informlinda@gmail.com>
2025-08-06 16:20:51 -0700

    Added tailwind config file along with set up css for tailwind input, screenshot for template, and compiled styles.

--- header.php
+++ header.php
@@ -1,12 +1,21 @@
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php wp_title(); ?>Leeside Coaching</title>
-  <?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title(); ?>Leeside Coaching</title>
+    <?php wp_head(); ?>
 </head>
 
-<?php
-$front_page_id = get_option('page_on_front');
-$front_page = get_post($front_page_id);
-echo apply_filters('the_content', $front_page->post_content);
-?>
+<div class="grid grid-cols-2 grid-rows-5 min-h-screen">
+    <div class="col-span-1 row-start-2 pl-40">
+        <div class="prose max-w-none">
+            <?php
+            $front_page_id = get_option('page_on_front');
+            $front_page = get_post($front_page_id);
+            echo apply_filters('the_content', $front_page->post_content);
+            ?>
+        </div>
+    </div>
+
+    <div class="col-span-1">
+    </div>
+</div>
