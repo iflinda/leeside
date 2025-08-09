ca80da076d5ef3c7877f87ceed63240baf8033e5
iflinda <informlinda@gmail.com>
2025-08-09 07:14:37 -0700

    Added logo file and seperated css for nav. Fixed the spacing where php is used to align with wordpress guides.

--- functions.php
+++ functions.php
@@ -2,9 +2,10 @@
 function leeside_theme_setup() {
     add_theme_support( 'title-tag' );
 }
-    add_action( 'after_setup_theme', 'leeside_theme_setup' );
 
-    require get_template_directory() . '/inc/setup.php';
-    require get_template_directory() . '/inc/enqueue.php';
-    require get_template_directory() . '/inc/custom-post-types.php';
+add_action( 'after_setup_theme', 'leeside_theme_setup' );
+
+require get_template_directory() . '/inc/setup.php';
+require get_template_directory() . '/inc/enqueue.php';
+require get_template_directory() . '/inc/custom-post-types.php';
 ?>
--- template-parts/menu.php
+++ template-parts/menu.php
@@ -2,24 +2,22 @@
 $menu_page = get_page_by_title('Menu');
 $menu_id = $menu_page ? $menu_page->ID : null;
 
-$logo = get_field('logo_image', $menu_id);
-$meet = get_field('meet_button', $menu_id);
-$sessions = get_field('sessions_button', $menu_id);
-$testimonials = get_field('testimonials_button', $menu_id);
-$contact = get_field('contact_button', $menu_id);
+// $logo = $menu_id ? get_field('logo_image', $menu_id) : null;
+$meet = $menu_id ? get_field('meet_button', $menu_id) : '';
+$sessions = $menu_id ? get_field('sessions_button', $menu_id) : '';
+$testimonials = $menu_id ? get_field('testimonials_button', $menu_id) : '';
+$contact = $menu_id ? get_field('contact_button', $menu_id) : '';
 ?>
 
-<nav class="site-nav">
+<nav class="px-8 flex justify-between items-end">
   <a href="#hero" class="logo">
-    <?php if ($logo): ?>
-      <img src="<?php echo esc_url($logo['url']); ?>" alt="Site Logo">
-    <?php endif; ?>
+    <img src="/wp-content/themes/leeside/assets/images/logo.svg" alt="Site Logo" class="h-10 w-auto">
   </a>
 
-  <ul class="nav-links">
-    <li><a href="#meet"><?php echo esc_html($meet); ?></a></li>
-    <li><a href="#sessions"><?php echo esc_html($sessions); ?></a></li>
-    <li><a href="#testimonials"><?php echo esc_html($testimonials); ?></a></li>
-    <li><a href="#contact"><?php echo esc_html($contact); ?></a></li>
+  <ul class="nav-links flex space-x-8">
+    <li><a href="#meet" class="hover:text-brandBlue"><?php echo esc_html($meet); ?></a></li>
+    <li><a href="#sessions" class="hover:text-brandBlue"><?php echo esc_html($sessions); ?></a></li>
+    <li><a href="#testimonials" class="hover:text-brandBlue"><?php echo esc_html($testimonials); ?></a></li>
+    <li><a href="#contact" class="hover:text-brandBlue"><?php echo esc_html($contact); ?></a></li>
   </ul>
 </nav>
